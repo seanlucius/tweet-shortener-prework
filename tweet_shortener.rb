1f362282be5ab9d1c8ea7893d2a3c29719a2926d
Sean Lucius <seanlucius22@icloud.com>
2019-01-25 01:11:17 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,38 @@
-# Write your code here.
+def dictionary
+  { "hello" => 'hi', "to" => '2', "two" => '2', "too" => '2',
+  "for" => '4', "four" => '4', 'be' => 'b', 'you' => 'u', "at" => "@", "and" => "&" }
+end 
+
+def word_substituter(tweet)
+  tweet_words = tweet.split
+  dictionary_hash = dictionary 
+  dictionary_hash.each do |key, value|
+    tweet_words.each do |word|
+      if word.downcase == key 
+        word.replace(value)
+      end 
+    end 
+  end 
+  tweet_words.join(' ')
+end 
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end 
+end 
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end 
+end 
+
+def shortened_tweet_truncator(tweet)
+  selected = selective_tweet_shortener(tweet)
+  if selected > 140
+    
+  
+end 
